db9a08cf28e232a19eb773b5e63ae26b27936f38
Tadeu Tupinambá <tadeu.tupiz@gmail.com>
2022-06-11 17:37:14 -0300

    pagination

--- dashboard/src/pages/users/index.tsx
+++ dashboard/src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Siderbar";
 
 export default function UserList() {
@@ -84,6 +85,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
